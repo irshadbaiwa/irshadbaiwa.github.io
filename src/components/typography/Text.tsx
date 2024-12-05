4ac6e93539ca765c0df38b5c4592bcdb4ab6522a
Abubakar Baiwa <irshaadbaiwa@gmail.com>
2024-12-06 00:11:47 +0100

    change text color

--- src/components/typography/Text.tsx
+++ src/components/typography/Text.tsx
@@ -6,9 +6,7 @@ interface Props {
 
 const Text: React.FC<Props> = ({ children }) => {
   // text-[#637381]
-  return (
-    <p className="text-base md:text-lg text-gray-400 font-medium">{children}</p>
-  )
+  return <p className="text-base md:text-lg text-gray-600 ">{children}</p>
 }
 
 export default Text
